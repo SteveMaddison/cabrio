bf305ac16a35a921a55de6d423edc09af8c386e0
Steve Maddison <steve@cosam.org>
2009-10-24 17:01:36 +0000

    Event autorepeat mechanism added

--- event.c
+++ event.c
@@ -16,6 +16,7 @@ static const int BALL_THRESHOLD = 100;
 static const int MOUSE_THRESHOLD = 100;
 static int num_joysticks = 0;
 static SDL_Joystick *joysticks[MAX_JOYSTICKS];
+static int event = EVENT_NONE;
 
 extern struct config* config;
 
@@ -51,90 +52,91 @@ int event_resume( void ) {
 
 int event_get( void ) {
 	struct config_control *control = config->iface.controls;
-	SDL_Event event;
+	SDL_Event sdl_event;
 
-	if( event.type == SDL_QUIT ) {
-		return EVENT_QUIT;
+	if(	SDL_PollEvent( &sdl_event ) ) {
+		event = EVENT_NONE;
+		if( sdl_event.type == SDL_QUIT ) {
+			event = EVENT_QUIT;
+		}
+		while( control ) {
+			if( control->device_type == DEV_KEYBOARD ) {
+				if( sdl_event.type == SDL_KEYDOWN && sdl_event.key.keysym.sym == control->value ) {
+					event = control->event;
+				}
+			}
+			else if( control->device_type == DEV_JOYSTICK ) {
+				if( sdl_event.type == SDL_JOYAXISMOTION
+				&&  sdl_event.jaxis.which == control->device_id
+				&&  control->control_type == CTRL_AXIS
+				&&  sdl_event.jaxis.axis == control->control_id ) {
+					if( sdl_event.jaxis.value < -AXIS_THRESHOLD && control->value < 0 ) {
+						event = control->event;
+					}
+					else if ( sdl_event.jaxis.value > AXIS_THRESHOLD && control->value > 0 ) {
+						event = control->event;
+					}
+				}
+				else if ( sdl_event.type == SDL_JOYBUTTONDOWN
+				&&  sdl_event.jbutton.which == control->device_id
+				&&  control->control_type == CTRL_BUTTON
+				&&  sdl_event.jbutton.button == control->value ) {
+					event = control->event;
+				}
+				else if ( sdl_event.type == SDL_JOYHATMOTION
+				&&  sdl_event.jhat.which == control->device_id
+				&&  control->control_type == CTRL_HAT
+				&&  sdl_event.jhat.hat == control->control_id
+				&&  sdl_event.jhat.value == control->value ) {
+					event = control->event;
+				}
+				else if ( sdl_event.type == SDL_JOYBALLMOTION
+				&&  sdl_event.jball.which == control->device_id
+				&&  control->control_type == CTRL_BALL
+				&&  sdl_event.jball.ball == control->control_id ) {
+					if( control->value == DIR_DOWN && sdl_event.jball.yrel > BALL_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_UP && sdl_event.jball.yrel < -BALL_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_LEFT && sdl_event.jball.xrel < -BALL_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_RIGHT && sdl_event.jball.xrel > BALL_THRESHOLD ) {
+						event = control->event;
+					}
+				}		
+			}
+			else if( control->device_type == DEV_MOUSE ) {
+				if( sdl_event.type == SDL_MOUSEBUTTONDOWN 
+				&& sdl_event.button.which == control->device_id
+				&& control->control_type == CTRL_BUTTON
+				&& sdl_event.button.button == control->value ) {
+					event = control->event;
+				}
+				else if ( sdl_event.type == SDL_MOUSEMOTION
+				&& sdl_event.motion.which == control->device_id
+				&& control->control_type == CTRL_AXIS ) {
+					if( control->value == DIR_UP && sdl_event.motion.yrel > MOUSE_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_DOWN && sdl_event.motion.yrel < -MOUSE_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_LEFT && sdl_event.motion.xrel < -MOUSE_THRESHOLD ) {
+						event = control->event;
+					}
+					else if( control->value == DIR_RIGHT && sdl_event.motion.xrel > MOUSE_THRESHOLD ) {
+						event = control->event;
+					}
+				}
+			}
+			control = control->next;
+		}
 	}
 	
-	SDL_PollEvent( &event );
-	while( control ) {
-		if( control->device_type == DEV_KEYBOARD ) {
-			if( event.type == SDL_KEYDOWN && event.key.keysym.sym == control->value ) {
-				return control->event;
-			}
-		}
-		else if( control->device_type == DEV_JOYSTICK ) {
-			if( event.type == SDL_JOYAXISMOTION
-			&&  event.jaxis.which == control->device_id
-			&&  control->control_type == CTRL_AXIS
-			&&  event.jaxis.axis == control->control_id ) {
-				if( event.jaxis.value < -AXIS_THRESHOLD && control->value < 0 ) {
-					return control->event;
-				}
-				else if ( event.jaxis.value > AXIS_THRESHOLD && control->value > 0 ) {
-					return control->event;
-				}
-			}
-			else if ( event.type == SDL_JOYBUTTONDOWN
-			&&  event.jbutton.which == control->device_id
-			&&  control->control_type == CTRL_BUTTON
-			&&  event.jbutton.button == control->value ) {
-				return control->event;
-			}
-			else if ( event.type == SDL_JOYHATMOTION
-			&&  event.jhat.which == control->device_id
-			&&  control->control_type == CTRL_HAT
-			&&  event.jhat.hat == control->control_id
-			&&  event.jhat.value == control->value ) {
-				return control->event;
-			}
-			else if ( event.type == SDL_JOYBALLMOTION
-			&&  event.jball.which == control->device_id
-			&&  control->control_type == CTRL_BALL
-			&&  event.jball.ball == control->control_id ) {
-				if( control->value == DIR_DOWN && event.jball.yrel > BALL_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_UP && event.jball.yrel < -BALL_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_LEFT && event.jball.xrel < -BALL_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_RIGHT && event.jball.xrel > BALL_THRESHOLD ) {
-					return control->event;
-				}
-			}		
-		}
-		else if( control->device_type == DEV_MOUSE ) {
-			if( event.type == SDL_MOUSEBUTTONDOWN 
-			&& event.button.which == control->device_id
-			&& control->control_type == CTRL_BUTTON
-			&& event.button.button == control->value ) {
-				return control->event;
-			}
-			else if ( event.type == SDL_MOUSEMOTION
-			&& event.motion.which == control->device_id
-			&& control->control_type == CTRL_AXIS ) {
-				if( control->value == DIR_UP && event.motion.yrel > MOUSE_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_DOWN && event.motion.yrel < -MOUSE_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_LEFT && event.motion.xrel < -MOUSE_THRESHOLD ) {
-					return control->event;
-				}
-				else if( control->value == DIR_RIGHT && event.motion.xrel > MOUSE_THRESHOLD ) {
-					return control->event;
-				}
-			}
-		}
-		control = control->next;
-	}
-	
-	return EVENT_NONE;
+	return event;
 }
 
 int event_probe( int timeout ) {
