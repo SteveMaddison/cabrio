436e5c5c028d7dd026d9fb1227c0702496e956e8
Steve Maddison <steve@cosam.org>
2009-11-20 15:35:31 +0000

    Video scaling/looping

--- video.c
+++ video.c
@@ -4,9 +4,10 @@
 #include "video.h"
 #include "ogl.h"
 
-static const int VIDEO_SIZE = 512;
+static const int VIDEO_SIZE = 256;
 static const int CONV_FORMAT = PIX_FMT_RGB24;
 static const int VIDEO_BPP = 3;
+static const GLfloat VIDEO_SCALE = 0.005;
 
 AVFormatContext *format_context = NULL;
 AVCodecContext *codec_context = NULL;
@@ -18,6 +19,7 @@ uint8_t *buffer;
 int video_stream = -1;
 struct SwsContext *scale_context;
 GLuint texture;
+GLfloat xsize, ysize;
 
 
 int video_init( void ) {
@@ -111,6 +113,13 @@ int video_open( const char *filename ) {
 	}
 	else {
 		int bytes = avpicture_get_size( CONV_FORMAT, VIDEO_SIZE, VIDEO_SIZE );
+		
+		printf( "%d x %d, %d.%d\n", codec_context->width, codec_context->height,
+			codec_context->time_base.num, codec_context->time_base.den );
+		
+		xsize = ((GLfloat)codec_context->width) * VIDEO_SCALE / 2.0;
+		ysize = ((GLfloat)codec_context->height) * VIDEO_SCALE / 2.0;
+		
 		buffer = (uint8_t*)av_malloc( bytes *sizeof(uint8_t) );
 	}
 	
@@ -124,59 +133,67 @@ int video_open( const char *filename ) {
 	return 0;
 }
 
+void video_rewind( void ) {
+	if( !format_context ) {
+		return;
+	}
+	else {
+		av_seek_frame( format_context, 0, 0, 0 );
+	}
+}
+
 int video_get_frame( void ) {
 	int got_frame = 0;
 	GLenum error = GL_NO_ERROR;
-	static int skip = 0;
 	
-	if( !format_context || !buffer )
+	if( !format_context || !codec_context || !buffer )
 		return -1;
 		
-	while( av_read_frame( format_context, &packet ) >= 0 ) {
+	if( av_read_frame( format_context, &packet ) >= 0 ) {
 		if( packet.stream_index == video_stream ) {
 			avcodec_decode_video( codec_context, raw_frame, &got_frame, packet.data, packet.size );
 		
 			if( got_frame ) {
-				if( ++skip == 5 ) {
-					skip = 0;
-					scale_context = sws_getCachedContext( scale_context, codec_context->width, codec_context->height, codec_context->pix_fmt,
-						VIDEO_SIZE, VIDEO_SIZE, CONV_FORMAT, SWS_BICUBIC, NULL, NULL, NULL );
-					if( !scale_context ) {
-						fprintf( stderr, "Warning: Couldn't initialise video conversion context\n" );
-						return -1;
-					}
-	
-					sws_scale( scale_context, raw_frame->data, raw_frame->linesize, 0,
-						VIDEO_SIZE, conv_frame->data, conv_frame->linesize );
-	
-					glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-					glBindTexture( GL_TEXTURE_2D, texture );
-					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-					glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-					glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-					glTexImage2D( GL_TEXTURE_2D, 0, VIDEO_BPP, VIDEO_SIZE, VIDEO_SIZE, 0,
-						GL_RGB, GL_UNSIGNED_BYTE, conv_frame->data[0] );
+				scale_context = sws_getCachedContext( scale_context, codec_context->width, codec_context->height,
+					codec_context->pix_fmt, VIDEO_SIZE, VIDEO_SIZE, CONV_FORMAT, SWS_BICUBIC, NULL, NULL, NULL );
+				if( !scale_context ) {
+					fprintf( stderr, "Warning: Couldn't initialise video conversion context\n" );
+					return -1;
 				}
+
+				sws_scale( scale_context, raw_frame->data, raw_frame->linesize, 0,
+					VIDEO_SIZE, conv_frame->data, conv_frame->linesize );
 				
-				ogl_load_alterego();
-				glTranslatef( 0.0, 0.0, -5 );
-				glEnable(GL_TEXTURE_2D);
-				glBegin( GL_QUADS );
-					glTexCoord2f(0.0, 0.0); glVertex3f(-2,  2, 0.0);
-					glTexCoord2f(0.0, 1.0); glVertex3f(-2, -2, 0.0);
-					glTexCoord2f(1.0, 1.0); glVertex3f( 2, -2, 0.0);
-					glTexCoord2f(1.0, 0.0); glVertex3f( 2,  2, 0.0);
-				glEnd();
-				
-				error = glGetError();
-				if( error != GL_NO_ERROR ) {
-					fprintf(stderr, "Warning: couldn't create texture: %s.\n", gluErrorString(error) );
-				}
+				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+				glBindTexture( GL_TEXTURE_2D, texture );
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+				glTexImage2D( GL_TEXTURE_2D, 0, VIDEO_BPP, VIDEO_SIZE, VIDEO_SIZE, 0,
+					GL_RGB, GL_UNSIGNED_BYTE, conv_frame->data[0] );
 			}
 			av_free_packet(&packet);
 		}
-		return 0;
 	}
-	return -1;
+	else {
+		video_rewind();	
+	}
+	
+	ogl_load_alterego();
+	glTranslatef( 0.0, 0.0, -5 );
+	glEnable(GL_TEXTURE_2D);
+	glBegin( GL_QUADS );
+		glTexCoord2f(0.0, 0.0); glVertex3f(-xsize,  ysize, 0.0);
+		glTexCoord2f(0.0, 1.0); glVertex3f(-xsize, -ysize, 0.0);
+		glTexCoord2f(1.0, 1.0); glVertex3f( xsize, -ysize, 0.0);
+		glTexCoord2f(1.0, 0.0); glVertex3f( xsize,  ysize, 0.0);
+	glEnd();
+	
+	error = glGetError();
+	if( error != GL_NO_ERROR ) {
+		fprintf(stderr, "Warning: couldn't create texture: %s.\n", gluErrorString(error) );
+	}
+	
+	return 0;
 }
